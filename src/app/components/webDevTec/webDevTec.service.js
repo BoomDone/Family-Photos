5034bd290c2afc59e85f8ff9d1901d3c04b16b9b
km13 <kmuehlenthaler@gmail.com>
2015-09-14 21:35:40 -0500

    Adding photoswipe to view the images larger

--- src/app/components/webDevTec/webDevTec.service.js
+++ src/app/components/webDevTec/webDevTec.service.js
@@ -9,36 +9,42 @@
   function webDevTec() {
     var data = [
      {
-      'location': 'MN Home',
+      'location': 'MN',
       'people': [
         'Kristen',
         'Eevee'
       ],
-      'eventItem':"ISU Game",
-      'date': 'September 2013',
-      'imgSrc': 'kristenEeveeISu.jpg'
+      'eventItem':"ISU-Game",
+      'date': 'September-2013',
+      'imgSrc': 'kristenEeveeISu.jpg',
+      'width':'683',
+      'height':'1024'
     },
     {
-      'location': 'MN Home',
+      'location': 'MN',
       'people': [
         'Kristen',
         'Smokey'
       ],
-      'eventItem':"New Kitty",
-      'date':"June 2012",
-      'imgSrc': 'kristenSmokey.jpg'
+      'eventItem':"New-Kitty",
+      'date':"June-2012",
+      'imgSrc': 'kristenSmokey.jpg',
+      'width':'1024',
+      'height':'683'
     },
     {
-      'location': 'MN Home',
+      'location': 'MN',
       'people': [
         'Bandit'
       ],
       'eventItem':"",
-      'date':"November 2012",
-      'imgSrc': 'bandit.jpg'
+      'date':"November-2012",
+      'imgSrc': 'bandit.jpg',
+      'width':'1024',
+      'height':'683'
     },
     {
-      'location': 'Iowa Home',
+      'location': 'Iowa',
       'people': [
         'Kristen',
         'Smokey',
@@ -47,19 +53,23 @@
         'Nick'
       ],
       'eventItem':"Christmas",
-      'date':"December 2014",
-      'imgSrc': 'christmas.jpg'
+      'date':"December-2014",
+      'imgSrc': 'christmas.jpg',
+      'width':'683',
+      'height':'1024'
     },
     {
-      'location': 'Iowa Home',
+      'location': 'Iowa',
       'people': [
         'Kyle',
         'Bandit',
         'Smokey'
       ],
       'eventItem':"",
-      'date':"July 2012",
-      'imgSrc': 'kylekitties.jpg'
+      'date':"July-2012",
+      'imgSrc': 'kylekitties.jpg',
+      'width':'1024',
+      'height':'683'
     }
     ];
 
